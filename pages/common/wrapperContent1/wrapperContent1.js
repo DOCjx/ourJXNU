8201c4d9600b3a3256fb6facabdebd3c2d8c827a
DOCjx <1324121459@qq.com>
2017-12-09 14:00:34 +0800

    update & add admin

---
+++ pages/common/wrapperContent1/wrapperContent1.js
@@ -0,0 +1,317 @@
+//wrapperContent.js
+Page({
+  data:{
+  	tagArr:[
+	  	{
+	  		title:"猜你喜欢",
+      	tags:[
+      		"数据结构15级试卷A.docx","数据结构15级试卷B.doc","数字逻辑课后习题答案.doc","高数课后习题答案.doc","15考研真题.pdf","计算机组成原理实验仪器介绍及实验报告.doc","计算机网络数据统计.xlsx","思修题库.doc","马原题库.doc","近代史题库.pdf","c语言实验报告.pdf","java从入门到放弃.pdf","模电.docx","马云的说话之道.pdf","乔布斯传.pdf"
+      	]
+	    },
+	  	{
+  			nav:"计算机信息工程学院",
+  			navId:1,
+  			navC:[
+  				{
+  					title:"数据结构",
+  					titleId:1
+  				},
+  				{
+  					title:"c语言",
+  					titleId:2
+  				},
+  				{
+  					title:"计算机网路",
+  					titleId:3
+  				},
+  				{
+  					title:"数字逻辑",
+  					titleId:4
+  				},
+  			]
+  		},{
+  			nav:"公共必修",
+  			navId:2,
+  			navC:[
+  				{
+  					title:"马克思主义原理",
+  					titleId:1
+  				},
+  				{
+  					title:"近代史纲要",
+  					titleId:2
+  				},
+  				{
+  					title:"思想品德与道德修养",
+  					titleId:3
+  				},
+  			]
+  		},{
+  			nav:"其它",
+  			navId:3,
+  			navC:[
+  				{
+  					title:"电子商务",
+  					titleId:1
+  				},
+  				{
+  					title:"经典心理电影欣赏",
+  					titleId:2
+  				},
+  				{
+  					title:"野外生存与技巧",
+  					titleId:3
+  				},
+  				{
+  					title:"西藏旅游与野外生存",
+  					titleId:4
+  				},
+  			]
+  		},
+  		{
+  			nav:"文学院",
+  			navId:4,
+  			navC:[
+  				{
+  					title:"aaa",
+  					titleId:1
+  				},
+  				{
+  					title:"bbb",
+  					titleId:2
+  				},
+  				{
+  					title:"ccc",
+  					titleId:3
+  				},
+  				{
+  					title:"ddd",
+  					titleId:4
+  				},
+  			]
+  		},
+  	],
+    contentArr:[
+      {navId:1,tags:[
+      	{titleId: 1,content:[
+      		{
+      			fiel:"数据结构14级试卷B.doc",
+      			fielId:1
+      		},
+      		{
+      			fiel:"数据结构13级试卷B.doc",
+      			fielId:2
+      		},
+      		{
+      			fiel:"数据结构考研真题.pdf",
+      			fielId:3
+      		}
+      	]},
+      	{titleId: 2,content:[,
+      		{
+      			fiel: "c语言实验报告1.doc",
+      			fielId:1
+      		},
+      		{
+      			fiel: "c语言实验报告2.docx",
+      			fielId:2
+      		},
+      		{
+      			fiel: "c语言实验报告6.doc",
+      			fielId:3
+      		},
+      		{
+      			fiel: "c语言上机真题.txt",
+      			fielId:4
+      		}
+      	]},
+      	{titleId: 3,content:[,
+      		{
+      			fiel: "12级计算机网络复习提纲.docx",
+      			fielId:1
+      		},
+      		{
+      			fiel: "计算机网络复习要点（整理）.doc",
+      			fielId:2
+      		}
+      	]},
+      	{titleId: 4,content:[,
+      		{
+      			fiel: "数字逻辑实验报告",
+      			fielId:1
+      		},
+      		{
+      			fiel: "数字逻辑课后习题校对",
+      			fielId:2
+      		}
+      	]}
+      ]},
+      {navId:2,tags:[
+      	{titleId: 1,content:[,
+      		{
+      			fiel: "a.doc",
+      			fielId:1
+      		},
+      		{
+      			fiel: "b.doc",
+      			fielId:2
+      		},
+      		{
+      			fiel: "c.pdf",
+      			fielId:3
+      		}
+      	]},
+      	{titleId: 2,content:[,
+      		{
+      			fiel: "sadf.doc",
+      			fielId:1
+      		},
+      		{
+      			fiel: "a.docx",
+      			fielId:2
+      		},
+      		{
+      			fiel: "asd.doc",
+      			fielId:3
+      		},
+      		{
+      			fiel: "asdf.txt",
+      			fielId:4
+      		}
+      	]},
+      	{titleId: 3,content:[,
+      		{
+      			fiel: "age.docx",
+      			fielId:1
+      		},
+      		{
+      			fiel: "wgtf.doc",
+      			fielId:2
+      		}
+      	]}
+      ]},{navId:3,tags:[
+      	{titleId: 1,content:[,
+      		{
+      			fiel: "afd.doc",
+      			fielId:1
+      		},
+      		{
+      			fiel: "dfs.doc",
+      			fielId:2
+      		},
+      		{
+      			fiel: "sdgasdgd.pdf",
+      			fielId:3
+      		}
+      	]},
+      	{titleId: 2,content:[,
+      		{
+      			fiel: "titleIdshjy.doc",
+      			fielId:1
+      		},
+      		{
+      			fiel: "hgdafdsfghrf.docx",
+      			fielId:2
+      		},
+      		{
+      			fiel: "dsfdsafdddc.doc",
+      			fielId:3
+      		},
+      		{
+      			fiel: "sdagaesrfedfs.txt",
+      			fielId:4
+      		}
+      	]},
+      	{titleId: 3,content:[,
+      		{
+      			fiel: "dsaf.docx",
+      			fielId:1
+      		},
+      		{
+      			fiel: "agcxgvfd.doc",
+      			fielId:2
+      		}
+      	]},
+      	{titleId: 4,content:[,
+      		{
+      			fiel: "fdger.xls",
+      			fielId:1
+      		},
+      		{
+      			fiel: "dsf.docx",
+      			fielId:2
+      		}
+      	]}
+      ]},{navId:4,tags:[
+      	{titleId: 1,content:[,
+      		{
+      			fiel: "sdgasdgd.pdf",
+      			fielId:1
+      		},
+      		{
+      			fiel: "dfs.doc",
+      			fielId:1
+      		},
+      		{
+      			fiel: "afd.doc",
+      			fielId:1
+      		}
+      	]},
+      	{titleId: 2,content:[,
+      		{
+      			fiel: "hgdafdsfghrf.docx",
+      			fielId:1
+      		},
+      		{
+      			fiel: "titleIdshjy.doc",
+      			fielId:2
+      		},
+      		{
+      			fiel: "dsfdsafdddc.doc",
+      			fielId:3
+      		},
+      		{
+      			fiel: "sdagaesrfedfs.txt",
+      			fielId:4
+      		}
+      	]},
+      	{titleId: 3,content:[,
+      		{
+      			fiel: "dsaf.docx",
+      			fielId:1
+      		},
+      		{
+      			fiel: "agcxgvfd.doc",
+      			fielId:2
+      		}
+      	]},
+      	{titleId: 4,content:[,
+      		{
+      			fiel: "fdger.xls",
+      			fielId:1
+      		},
+      		{
+      			fiel: "dsf.docx",
+      			fielId:2
+      		}
+      	]}
+      ]},
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    console.log(this.data)
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
